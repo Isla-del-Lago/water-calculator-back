1bd3a76f633b0ab00e4da4823fc06d166ad2a25a
Johan Camilo Lopez <camilino123@gmail.com>
2020-12-09 19:53:58 -0500

    Database config done, connection succesful and necessary dependencies added

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,1 +1,14 @@
-import Sequelize from 'sequelize';
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DATABASE_USER: string = process.env.DATABASE_USER!;
+const DATABASE_PASSWORD: string = process.env.DATABASE_PASSWORD!;
+const DATABASE_HOST: string = process.env.DATABASE_HOST!;
+const DATABASE_NAME = 'consumo_lagos';
+
+export const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, {
+    host: DATABASE_HOST,
+    dialect: 'mysql'
+});
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import Express from 'express';
 import dotenv from 'dotenv';
+import { sequelize } from './config/database';
 
 dotenv.config();
 
 const app = Express();
 
-const serverPort = process.env.SERVER_PORT || 3000;
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
-app.listen(serverPort, () => console.log(`Server listening on port ${serverPort}`));
+app.listen(SERVER_PORT, () => {
+    sequelize.authenticate()
+        .then(() => {
+            console.log('Database connection succesfull.');
+            console.log(`Server listening on port ${SERVER_PORT}`);
+        })
+        .catch(console.log)
+});

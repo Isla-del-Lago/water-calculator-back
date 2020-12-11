c02f8312d2e5703a82af0b52d100e0a306e6633c
Johan Camilo Lopez <camilino123@gmail.com>
2020-12-10 19:34:54 -0500

    Use valor unitario routes on express app

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import Express from 'express';
 import dotenv from 'dotenv';
 import { sequelize } from './config/database';
+import valorUnitarioRoutes from './routes/valorUnitarioRoutes';
 
 dotenv.config();
 
 const app = Express();
 
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
+
+app.use('/valorunitario', valorUnitarioRoutes);
 
 app.listen(SERVER_PORT, () => {
     sequelize.authenticate()

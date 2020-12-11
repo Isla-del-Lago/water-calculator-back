85c0bd03b02aa9cea72fd46f68321eaac26935f3
Johan Camilo Lopez <camilino123@gmail.com>
2020-12-11 18:38:07 -0500

    src/util/Consumo.ts

---
+++ src/util/Consumo.ts
@@ -0,0 +1,9 @@
+export default interface Consumo {
+    id: number,
+    num_apt: string,
+    bill_date: string,
+    consumption_price: number,
+    m3_rsd_bsc: number,
+    m3_rsd_bsc_sup: number,
+    counter_value: number
+}

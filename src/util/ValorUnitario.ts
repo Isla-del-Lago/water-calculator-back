99cac5ad483137120a43f442a37a8bbcad159461
Johan Camilo Lopez <camilino123@gmail.com>
2020-12-10 19:37:24 -0500

    Useful interfaces to treat values created

---
+++ src/util/ValorUnitario.ts
@@ -0,0 +1,13 @@
+export default interface ValorUnitario {
+    bill_date: string,
+    m3_rsd_bsc: number,
+    m3_rsd_bsc_sup: number,
+    disscounts: number,
+    acue_fijo_resd: number,
+    acue_rsd_bsc: number,
+    acue_rsd_bsc_sup: number,
+    alc_fijo_resd: number,
+    alc_rsd_bsc: number,
+    alc_rsd_bsc_sup: number,
+    aseo_total: number
+}

4133cad912fa0c61e29365ed6a5a8003d42f537e
Johan Camilo Lopez <camilino123@gmail.com>
2020-12-30 10:05:52 -0500

    Apartment flow tests done, test coverage near to 100%

--- src/test/apartmentTest.ts
+++ src/test/apartmentTest.ts
@@ -5,52 +5,21 @@ import IApartamento from '../util/Apartamento';
 import { Apartamento } from '../model/Apartamento';
 import { app } from '../index';
 import chaiHttp from 'chai-http';
+import { Model, ModelCtor } from 'sequelize/types';
 
 const assert = chai.assert;
 const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Apartment service implementation', () => {
-    describe('Get apartment by its number function', () => {
+describe('Apartment implementation', () => {
+    describe('Get apartment by it\'s number', () => {
         const stub = sinon.stub(Apartamento, 'findOne');
 
-        it('Should return null when apt doesn\'t exists', async () => {
-            stub.withArgs({ where: { num_apt: '200' } }).returns(Promise.resolve(null));
-
-            assert.equal(null, await apartmentService.getApartamentoByNumber('200'));
-        });
-
-        it('Should return an object when apartment does exists', async () => {
+        it('Should return ok status and data', (done) => {
             const fakeModel = Apartamento.build({ num_apt: '214', owner_name: 'x', password: 'y' });
 
             stub.withArgs({ where: { num_apt: '214' } }).returns(Promise.resolve(fakeModel));
-
-            assert.isNotNull(await apartmentService.getApartamentoByNumber('214'));
-        });
-    });
-
-    describe('Save a new apartment function', () => {
-        const stub = sinon.stub(Apartamento, 'create');
-
-        it('Should return a model when the save is succesfull', async () => {
-            const fakeApartment: IApartamento = { num_apt: '214', owner_name: 'x', password: 'y' };
-
-            stub.withArgs(fakeApartment, { returning: false }).returns(Promise.resolve());
-
-            assert.equal(null, await apartmentService.saveApartamento(fakeApartment));
-        });
-    });
-});
-
-describe('Apartment controller implementation', () => {
-    describe('Get apartment by it\'s number controller', () => {
-        const stub = sinon.stub(apartmentService, 'getApartamentoByNumber');
-
-        it('Should return ok status and data', () => {
-            const fakeModel = Apartamento.build({ num_apt: '214', owner_name: 'x', password: 'y' });
-
-            stub.withArgs('214').returns(Promise.resolve(fakeModel));
 
             chai.request(app)
                 .get('/apartment/214')
@@ -60,43 +29,53 @@ describe('Apartment controller implementation', () => {
                     res.body.data.should.have.property('num_apt').eql('214');
                     res.body.data.should.have.property('owner_name').eql('x');
                     res.body.data.should.have.property('password').eql('y');
-                })
+                    done();
+                });
         });
 
-        it('Should return 404 status and error', () => {
-            stub.withArgs('200').returns(Promise.resolve(null));
+        it('Should return 404 status and error', (done) => {
+            stub.withArgs({ where: { num_apt: '200' } }).returns(Promise.resolve(null));
 
             chai.request(app)
-                .get('/apartament/200')
+                .get('/apartment/200')
                 .end((err, res) => {
                     res.should.have.status(404);
                     res.should.have.property('error');
+                    done();
+                });
+        });
+
+        it('Should return 500 status and error', (done) => {
+            stub.withArgs({ where: { num_apt: '200' } }).returns(Promise.reject('Error'));
+
+            chai.request(app)
+                .get('/apartment/200')
+                .end((err, res) => {
+                    res.should.have.status(500);
+                    done();
                 });
         });
     });
 
-    describe('Save new apartment controller', () => {
-        const stub = sinon.stub(apartmentService, 'saveApartamento');
+    describe('Save new apartment', () => {
+        const mock = sinon.mock(Apartamento);
 
-        it('Should have status code 201 and data', () => {
+        it('Should have status code 201 and data', (done) => {
             const fakeApartment: IApartamento = { num_apt: '214', owner_name: 'x', password: 'y' };
             const fakeModel = Apartamento.build({ num_apt: '214', owner_name: 'x', password: 'y' });
 
-            stub.withArgs(fakeApartment).returns(Promise.resolve(fakeModel));
+            mock.expects('create').withArgs(fakeApartment).returns(Promise.resolve(fakeModel));
 
             chai.request(app)
                 .post('/apartment')
                 .send(fakeApartment)
                 .end((err, res) => {
                     res.should.have.status(201);
-                    res.body.should.have.property('data');
-                    res.body.data.should.have.property('num_apt').eql('214');
-                    res.body.data.should.have.property('owner_name').eql('x');
-                    res.body.data.should.have.property('password').eql('y');
+                    done();
                 });
         });
 
-        it('Should have status code 400 and error data', () => {
+        it('Should have status code 400 and error data', (done) => {
             const fakeApartment = {};
 
             chai.request(app)
@@ -105,6 +84,20 @@ describe('Apartment controller implementation', () => {
                 .end((err, res) => {
                     res.should.have.status(400);
                     res.should.have.property('error');
+                    done();
+                });
+        });
+
+        it('Should have status code 500 and error', (done) => {
+            const fakeApartment: IApartamento = { num_apt: '214', owner_name: 'x', password: 'y' };
+            mock.expects('create').withArgs(fakeApartment).returns(Promise.reject());
+
+            chai.request(app)
+                .post('/apartment')
+                .send(fakeApartment)
+                .end((err, res) => {
+                    res.should.have.status(500);
+                    done();
                 });
         });
     });
